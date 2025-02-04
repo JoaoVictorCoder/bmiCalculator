cefd020c25b3eef8742b0ccffd678ea4047f48f4
jvrd1515 <jvrd1515@gmail.com>
2025-02-04 20:56:08 +0000

    reduce validation

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ const weightInput = document.getElementById("weightInput")
 const heightInput = document.getElementById("heightInput")
 
 
-const e = document.createElement("h1") //elemento h1 que contem o resultado
-e.setAttribute("id", "result") //adiciona um atributo ao elemento, no caso o elemento id de valor result
+const bmiResult = document.createElement("h1") //elemento h1 que contem o resultado
+bmiResult.setAttribute("id", "result") //adiciona um atributo ao elemento, no caso o elemento id de valor result
 
 function calculate() {
-    document.body.appendChild(e)
+    document.body.appendChild(bmiResult)
     let weight = parseFloat(weightInput.value)
     let height = parseFloat(heightInput.value / 100)
     let isHeightDecimal = (heightInput.value).toString().includes('.');
     
-    if(!isNaN(weight && height) && (weight > 0 && height > 0) && (weight < 300 && height < 300) && !isHeightDecimal){
+    if(!isNaN(weight && height) && ((weight && height) > 0) && (weight < 300 && height < 300) && !isHeightDecimal){
     let BMI = (weight) / (height * height)
 
     console.log(BMI.toFixed(2))
-    e.innerHTML = `BMI: ${BMI.toFixed(2)}`
+    bmiResult.innerHTML = `BMI: ${BMI.toFixed(2)}`
     }else{
         console.error("Please, use a valid number");
-        e.innerHTML = "Please, use a valid number"
+        bmiResult.innerHTML = "Please, use a valid number"
     }
 }
