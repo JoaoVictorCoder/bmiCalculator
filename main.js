5fb8b81cd7270f0e45bf7f8dcabdbeded283d306
jvrd1515 <jvrd1515@gmail.com>
2025-02-04 13:03:21 +0000

    Add more validations

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const weightInput = document.getElementById("weightInput")
 const heightInput = document.getElementById("heightInput")
 
+
 const e = document.createElement("h1") //elemento h1 que contem o resultado
 e.setAttribute("id", "result") //adiciona um atributo ao elemento, no caso o elemento id de valor result
 
@@ -8,13 +9,16 @@ function calculate() {
     document.body.appendChild(e)
     let weight = parseFloat(weightInput.value)
     let height = parseFloat(heightInput.value / 100)
+    let isWeightDecimal = weight.toString().includes('.' || ',');
+    let isHeightDecimal = height.toString().includes('.' || ',');
     
-    if(!isNaN(weight && height)){
+    if(!isNaN(weight && height) && (weight > 0 && height > 0) && (weight < 300 && height < 300) && (!isWeightDecimal || !isHeightDecimal)){
     let BMI = (weight) / (height * height)
+
     console.log(BMI.toFixed(2))
     e.innerHTML = `BMI: ${BMI.toFixed(2)}`
     }else{
-        console.error("You need to use numbers");
-        e.innerHTML = "You need to use numbers"
+        console.error("Please, use a valid number");
+        e.innerHTML = "Please, use a valid number"
     }
 }
